60f7b21614497e96b6f2d546e1a41d2b37fafc8b
RaphaelBoulat <r.boulat@icloud.com>
2023-02-13 23:31:25 +0100

    Not so conclusive :-1:

--- wrangling.R
+++ wrangling.R
@@ -6,6 +6,7 @@ testing <- read_csv("/Users/raphaelboulat/Desktop/Github/1636_competition_assign
 # Convert all columns to factor
 
 training <- as.data.frame(unclass(training), stringsAsFactors = TRUE) 
+testing <- as.data.frame(unclass(testing), stringsAsFactors = TRUE) 
 
 ### filter for weird numbers --> not sure how to treat income_a/.../income_d (they only have 2,3,4 and "No change" --> maybe kick them out?)
 
@@ -23,9 +24,29 @@ training_imputed <- missRanger(data = training,
                                formula = . ~ . -income - income_a -income_b -income_c -income_d, 
                                num.trees = 100)
 
+testing_imputed <- missRanger(data = testing,
+                              formula = . ~ . - income_a -income_b -income_c -income_d, 
+                              num.trees = 100)
 
 ### random forest 
 
 control <- trainControl(method = "cv", number=5)
-tuning_grid = expand.grid(mtry = seq(10, 20, by = 5), splitrule = "variance", min.node.size=500)
-rf_caret = caret::train(data = training_imputed , income ~ ., method = "ranger", tuneGrid = tuning_grid, trControl = control, importance="impurity")
+tuning_grid_rf = expand.grid(mtry = 5, splitrule = "variance", min.node.size=500)
+rf_caret = caret::train(data = training_imputed , income ~ ., method = "ranger", tuneGrid = tuning_grid_rf , trControl = control, importance="impurity")
+rf_1 <- rf_caret$finalModel
+rf_1
+
+pred_rf_1 <- predict(rf_caret, testing_imputed)
+Metrics::rmse(actual = training_imputed$income, predicted = pred_rf_1)
+
+### boosting 
+
+control = trainControl(method = "cv", number = 5)
+tuning_grid_xbg = expand.grid(nrounds = 100, max_depth = 3, eta = 0.4, gamma = 0.01, 
+                              colsample_bytree = 1, min_child_weight = 1, subsample = 1)
+gb_caret = caret::train(data = training_imputed, income ~ ., method = "xgbTree", trControl = control, tuneGrid = tuning_grid_xbg, verbosity = 0)
+gb_1 <- gb_caret$finalModel
+
+pred_gb_1 <- predict(gb_1 , newdata = testing_imputed)
+Metrics::rmse(actual = training_imputed$income, predicted = pred_gb_1)
+
